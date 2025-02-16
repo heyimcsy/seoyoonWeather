f3f948bb311e741a60ee721c5ea91da970e657b4
sychoi <cinnamon1995@naver.com>
2025-02-17 02:40:33 +0900

    View는 기본적으로 flexRow의 기능을 가지고 있고, 웹과 다르게 column 이 default

--- App.js
+++ App.js
@@ -4,14 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   const myName = 'Seoyoon';
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello {myName} made a ReactNative APP!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flexDirection: 'row' }}>
+      <View style={{ width: 100, height: 100, backgroundColor: 'tomato' }}></View>
+      <View style={{ width: 100, height: 100, backgroundColor: 'teal' }}></View>
+      <View style={{ width: 100, height: 100, backgroundColor: 'orange' }}></View>
+      {/* <Text style={styles.text}>Hello {myName} made a ReactNative APP!!!</Text> */}
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
-//STyleSheet을 쓰는 이점은 내장된 함수들을 자동완성 기능으로 보여줘서 편리
+//STyleSheet을 쓰는 이점은 내장된 함수들을 자동완성 기능으로 보여줘서 편리학기 때문이다.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
