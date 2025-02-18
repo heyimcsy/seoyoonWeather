4926d7241d9cf8c0c94d9fee45b928fa7b1f197f
sychoi <cinnamon1995@naver.com>
2025-02-18 22:44:32 +0900

    weatherapp css

--- App.js
+++ App.js
@@ -4,12 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   const myName = 'Seoyoon';
   return (
-    <View style={{ flex: 1, flexDirection: 'row' }}>
-      <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
-      <View style={{ flex: 2, backgroundColor: 'teal' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
-      {/* <Text style={styles.text}>Hello {myName} made a ReactNative APP!!!</Text> */}
-      {/* <StatusBar style="auto" /> */}
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -18,12 +22,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'tomato',
   },
-  text: {
-    fontSize: 28,
-    color: 'blue',
+  city: {
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: '500',
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
